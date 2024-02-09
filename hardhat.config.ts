68bb9466b8e34e166139a8e73a3fec52fe8cb580
Maratib Ali Khan <maratib@gmail.com>
2024-02-09 23:40:25 +0500

    config added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-toolbox-viem";
 import 'hardhat-watcher'
 
 const config: HardhatUserConfig = {
